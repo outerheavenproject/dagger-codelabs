335ab83f1471ebc807d313ae0ee4b7683ff88b7e
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-30 17:48:55 +0900

    simplify

--- scripts/codelab_gen.sh
+++ scripts/codelab_gen.sh
@@ -2,6 +2,7 @@
 # encoding:utf-8
 
 readonly file="/tmp/$(date +%s).md"
-cat $(find md -name "*.md"  | sort) > $file
+cat md/INDEX.md > $file
+cat md/chapters/*.md >> $file
 /tmp/claat export -o docs/ $file
 rm $file
