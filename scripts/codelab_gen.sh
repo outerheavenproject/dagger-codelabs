f9a5af5f5f37fd4b5bce3d9231252a4e6ba42e9b
Yoshihiro Tanaka <contact@cordea.jp>
2019-07-06 15:03:23 +0900

    fix deploy script

--- scripts/codelab_gen.sh
+++ scripts/codelab_gen.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # encoding:utf-8
 
-readonly file="/tmp/$(date +%s).md"
+readonly base_dir="/tmp/dagger_codelabs"
+readonly file="$base_dir/$(date +%s).md"
 
-cp md/chapters/*.png ./.
+mkdir $base_dir
+
+cp md/chapters/*.png "$base_dir/."
 
 cat md/INDEX.md > $file
 cat md/chapters/*.md >> $file
 /tmp/claat export -o docs/ $file
-rm $file
+
+rm -r $base_dir
